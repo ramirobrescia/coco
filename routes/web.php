16925e06cc194272b1b3034f99301c5562b343a7
Ramiro Brescia <ramiro.brescia@nano-apps.com>
2024-12-05 12:31:19 -0300

    Se protegieron las paginas con login

--- routes/web.php
+++ routes/web.php
@@ -7,31 +7,33 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Home');
+    })->name('home');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::resource('/proveedores', ProviderController::class);
+    Route::resource('/compras', PurchaseController::class);
 });
 
-// Purchase
-Route::get('/compras/dashboard', function () {
-    // return Inertia::render('CocoDashboard');
-    return Inertia::render('coco/Dashboard');
-});
-Route::resource('/providers', ProviderController::class);
-Route::resource('/compras', PurchaseController::class);
 
 require __DIR__.'/auth.php';
