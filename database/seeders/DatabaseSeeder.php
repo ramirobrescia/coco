a1ea8de526a80ab0bd7fc99afe8333b0d09330da
Ramiro Brescia <ramiro.brescia@nano-apps.com>
2024-12-05 07:46:35 -0300

    Database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Provider;
+use App\Models\Purchase;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $users = User::factory(5)->create();
+        $providers = Provider::factory(5)->create();
+
+        Task::factory()->createMany([
+            ['name' => 'Recibir pedido'],
+            ['name' => 'Realizar pedido'],
+            ['name' => 'Juntar dinero'],
+            ['name' => 'Juntar dinero'],
         ]);
+
+        $elements = collect($users);
+        $elements = $elements->merge($providers);
+        
+        $purchases = Purchase::factory(10)
+            ->recycle($elements)
+            ->create();
+
     }
 }
